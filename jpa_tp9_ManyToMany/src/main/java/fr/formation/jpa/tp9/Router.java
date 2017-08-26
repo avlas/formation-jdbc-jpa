b9719e47545ab31cf36ddbfd989960557c43853c
Avl <alina.vlas@gmail.com>
2017-08-27 01:31:07 +0200

    testing @ManyToMany

---
+++ jpa_tp9_ManyToMany/src/main/java/fr/formation/jpa/tp9/Router.java
@@ -0,0 +1,88 @@
+package fr.formation.jpa.tp9;
+
+import static spark.Spark.get;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import fr.formation.jpa.tp9.model.Commune;
+import fr.formation.jpa.tp9.model.Elu;
+import fr.formation.jpa.tp9.model.Maire;
+import fr.formation.jpa.tp9.model.Projet;
+import fr.formation.jpa.tp9.model.Secretaire;
+import freemarker.template.Configuration;
+import freemarker.template.Version;
+import spark.ModelAndView;
+import spark.servlet.SparkApplication;
+import spark.template.freemarker.FreeMarkerEngine;
+
+public class Router implements SparkApplication {
+	
+	public void init() {
+
+		get("/home", (request, response) -> {			
+			EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("election");
+			EntityManager em = entityManagerFactory.createEntityManager();
+
+			Commune commune1 = new Commune();
+			commune1.setNom("Commune1");
+					
+			Set<Projet> projets = new HashSet<Projet>();
+			Projet p1 = new Projet();
+			p1.setNom("Proj1");
+			projets.add(p1);
+			
+			Projet p2 = new Projet();
+			p2.setNom("Proj2");
+			projets.add(p2);
+			
+			Set<Elu> elus = new HashSet<Elu>();
+			Elu elu1 = new Elu();
+			elu1.setNom("Elu1");
+			elu1.setProjets(projets);
+			elus.add(elu1);
+			
+			Elu elu2 = new Elu();
+			elu1.setNom("Elu2");
+			elus.add(elu2);
+			
+			Maire maire1 = new Maire();
+			maire1.setNom("Maire1");			
+			maire1.setElu(elus);
+			commune1.setMaire(maire1);
+			
+			// commune1 is NULL in DB
+			// maire1.setCommune(commune1);
+			
+			Secretaire secretaire1 = new Secretaire();
+			secretaire1.setMaire(maire1);
+						
+			em.getTransaction().begin();
+			em.persist(elu1);
+			em.persist(elu2);
+			em.persist(maire1);
+			em.persist(secretaire1);
+			em.persist(commune1);
+			em.getTransaction().commit();
+			
+			Map<String, Object> attributes = new HashMap<>();
+			return new ModelAndView(attributes, "home.ftl");
+		}, getFreeMarkerEngine());
+	}
+	
+	private FreeMarkerEngine getFreeMarkerEngine() {
+		FreeMarkerEngine engine = new FreeMarkerEngine();
+		Configuration configuration = new Configuration(new Version(2, 3, 23));
+		configuration.setTemplateUpdateDelayMilliseconds(Long.MAX_VALUE);
+		configuration.setClassForTemplateLoading(FreeMarkerEngine.class, "");
+		engine.setConfiguration(configuration);
+		return engine;
+	}
+
+}
---
+++ jpa_tp9_ManyToMany/src/main/java/fr/formation/jpa/tp9/model/Elu.java
@@ -0,0 +1,47 @@
+package fr.formation.jpa.tp9.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="elu")
+public class Elu {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(length = 40)
+	private String nom;
+	
+	@ManyToMany
+	private Set <Projet> projets;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Set<Projet> getProjets() {
+		return projets;
+	}
+
+	public void setProjets(Set<Projet> projets) {
+		this.projets = projets;
+	}
+	
+}
